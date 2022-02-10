fd97d6bc6bfd48dca3109adb5d7d8dc01b209ff8
Chitsanupong <first30952@gmail.com>
2022-02-10 12:13:11 +0700

    feat: enable import svg as component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import { SidebarContainer, LogoContainer } from './Sidebar.styled';
+import { ReactComponent as Icon } from '../images/icon.svg';
 
 const Sidebar = () => {
   return (
     <SidebarContainer>
       <LogoContainer>
         <span>
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 38 47"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect y="23" width="8" height="24" fill="#fff" />
-            <rect x="15" y="12" width="8" height="35" fill="#fff" />
-            <rect x="30" width="8" height="47" fill="#fff" />
-          </svg>
+          <Icon />
         </span>
         <h2>Analytics</h2>
       </LogoContainer>
