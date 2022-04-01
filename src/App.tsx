dc9ab11526e1874a38546319bb7f95bc67e051f2
Chitsanupong <first30952@gmail.com>
2022-04-01 12:53:18 +0700

    feat: set up path resolving

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import BarChart from './components/BarChart';
-import { Container } from './components/Container.styled';
-import LineChart from './components/LineChart';
-import Sidebar from './components/Sidebar';
-import theme from './theme';
-import { HoverCard, GenerateQRCode } from './components';
+import BarChart from '@/components/BarChart';
+import { Container } from '@/components/Container.styled';
+import LineChart from '@/components/LineChart';
+import Sidebar from '@/components/Sidebar/Sidebar';
+import theme from '@/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <Sidebar />
-        <BarChart />
-        <LineChart />
-        <HoverCard />
-        <GenerateQRCode text="Chitsanupong Tangvasinkul" />
       </Container>
     </ThemeProvider>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,7 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 });
